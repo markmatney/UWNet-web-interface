90c13b4a6b1a12588fdd0cff083973a12f5b9b2c
eseguraca6 <eseguraca6@Cs-212-25.CS.UCLA.EDU>
2015-08-18 13:42:23 -0700

    serial test

--- tst.py
+++ tst.py
@@ -6,10 +6,15 @@ This is a temporary script file.
 """
 
 import subprocess
+import serial
 
-#configure the serial port by running:
-script_to_config = "kermit CFG_OFDM_38400"
-#TODO: run kermit ^^
+kermit_port_1 = serial.serial(port, '/dev/ttyUSB0', baudrate = 38400, parity = 'N', stopbits = 1, timeout = None, xonxoff = 0, rtscts = 0)
 
-p = subprocess.call(["kermit", "CFG_AQUASENT_KERMIT_38400" ,"-c","+++A"])
+kermit_port_2 = serial.serial(port, '/dev/ttyUSB1', baudrate = 38400, parity = 'N', stopbits = 1, timeout = None, xonxoff = 0, rtscts = 0)
+
+commands = ["+++A", "$HHCRW, TXPWR, 10"]
+
+for i in commands:
+    ser.write(i)
+
 

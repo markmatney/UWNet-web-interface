5b239ef60d9dad0afd814b6c6f172824c563de87
eseguraca6 <eseguraca6@gmail.com>
2015-08-31 14:35:13 -0700

    data python plotting script

--- extract_db_for_plot.py
+++ extract_db_for_plot.py
@@ -4,10 +4,16 @@ import time
 import hashlib #for checksum purposes
 import mysql.connector # mysql database
 import getpass
+from matplotlib import pyplot
+import pylab
+from mpl_toolkits.mplot3d import Axes3D
+import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 sys.path.append('/usr/lib/python2.7/dist-packages')
 
-cnx = mysql.connector.connect(user= getpass.getuser(), password='pass', host='localhost', database='UWNet')
+cnx = mysql.connector.connect(user= 'root', password='sylmar123', host='localhost', database='UWNet')
 
 cursor = cnx.cursor()
 cursor_insert = cnx.cursor()
@@ -35,15 +41,19 @@ for exp in [6,7,8,9,10,11,12,13,14,15]:
 # to extract data points for each plot:
 # example: plot data for experiment 6
 
-plot_data = data[6]
+plot_data = data[11]
 bkn_vals = plot_data['bkns']
 mod_vals = plot_data['mods']
 delay_vals = plot_data['delays']
 
-print plot_data
-print
-print bkn_vals
-print
-print mod_vals
-print
-print delay_vals
+fig = plt.figure()
+#ax2 = Axes3D(fig)
+ax = fig.add_subplot(111, projection ='3d')
+#X, Y = np.meshgrid(bkn_vals, mod_vals)
+ax.plot_contourf(bkn_vals, mod_vals, delay_vals)
+#ax2.view_init(28, -144) 
+plt.show()
+#ax.plot_wireframe(bkn_vals, mod_vals, delay_vals, rstride =1, cstride =1)
+#ax.axis
+#pyplot.show()
+#fig = plt.figure()
